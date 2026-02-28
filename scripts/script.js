ceea45f0d895f45bc5a8e09d9d753d68b161d3b1
Lihkit Vema <likhit.vermacs@gmail.com>
2026-02-28 17:57:48 +0530

    Updated content in Resume

--- scripts/script.js
+++ scripts/script.js
@@ -173,7 +173,7 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
     const target = document.querySelector(this.getAttribute("href"));
-    if (target) {
+    if (target) { 
       target.scrollIntoView({
         behavior: "smooth",
         block: "start",
